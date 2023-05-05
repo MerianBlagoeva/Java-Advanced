991e14573444a7f7f7418081db38b48012be5353
Merian <drundrun00@gmail.com>
2023-05-05 17:43:35 +0300

    Added MultidimensionalArrays exercises

---
+++ Java Advanced/MultidimensionalArrays/src/Lab/CompareMatrices.java
@@ -0,0 +1,71 @@
+package Lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CompareMatrices {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] firstMatrix = readMatrix(sc);
+        int[][] secondMatrix = readMatrix(sc);
+
+        boolean isEqual = compareMatrices(firstMatrix, secondMatrix);
+
+        String output = isEqual ? "equal" : "not equal";
+
+        System.out.println(output);
+
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] arr : matrix) {
+            for (int element : arr) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] readMatrix(Scanner sc) {
+        String[] dimensions = sc.nextLine().split("\\s+");
+        int rows = Integer.parseInt(dimensions[0]);
+        int cols = Integer.parseInt(dimensions[1]);
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            int[] arr1 = Arrays.stream(sc.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            matrix[row] = arr1;
+        }
+        return matrix;
+    }
+
+    public static boolean compareMatrices(int[][] firstMatrix, int[][] secondMatrix) {
+        if (firstMatrix.length != secondMatrix.length) {
+            return false;
+        }
+
+        for (int row = 0; row < firstMatrix.length; row++) {
+            int[] firstArr = firstMatrix[row];
+            int[] secondArr = secondMatrix[row];
+
+            if (firstArr.length != secondArr.length) {
+                return false;
+            }
+
+            for (int col = 0; col < firstArr.length; col++) {
+                int firstElement = firstArr[col];
+                int secondElement = secondArr[col];
+
+                if (firstElement != secondElement) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
