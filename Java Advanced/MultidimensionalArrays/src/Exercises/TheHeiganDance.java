2d0110a3e69731f05c4ff95d00fe54ecbb1d897c
Merian <drundrun00@gmail.com>
2023-05-20 14:24:13 +0300

    Added exercises

---
+++ Java Advanced/MultidimensionalArrays/src/Exercises/TheHeiganDance.java
@@ -0,0 +1,10 @@
+package Exercises;
+
+import java.util.Scanner;
+
+public class TheHeiganDance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
